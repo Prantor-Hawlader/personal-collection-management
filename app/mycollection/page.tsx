a3828764b6acf145b0424bc04acd70869bcce373
Prantor-Hawlader <prantor502@gmail.comm>
2024-08-30 00:44:30 +0600

    myCollection props issue solved

--- app/mycollection/page.tsx
+++ app/mycollection/page.tsx
@@ -29,8 +29,12 @@ const myCollection = async () => {
 
   return (
     <div className="w-full">
-      <AddNewCollection categories={categories} />
-      <CollectionTable categories={categories} collections={collections} />
+      <AddNewCollection categories={categories} userId={userId!} />
+      <CollectionTable
+        categories={categories}
+        collections={collections}
+        userId={userId!}
+      />
     </div>
   );
 };
